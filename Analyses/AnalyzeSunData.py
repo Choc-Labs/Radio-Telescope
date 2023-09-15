d7f5755051858f054f5da6d5e71592d527814908
Choc-a-Block <Choc-a-Block@users.noreply.github.com>
2023-09-15 18:50:56 +0000

    :art: Format Python code with psf/black

--- Analyses/AnalyzeSunData.py
+++ Analyses/AnalyzeSunData.py
@@ -16,10 +16,10 @@ def readMRT(filename):
     for item in dat.items():
         d[item[0]] = item[1]
     #   Determine active axis
-    if (np.median(np.diff(d['az'])) == 0):
-        d['aa'] = 'el'
+    if np.median(np.diff(d["az"])) == 0:
+        d["aa"] = "el"
     else:
-        d['aa'] = 'az'
+        d["aa"] = "az"
     return d
 
 
@@ -27,9 +27,19 @@ dat = {}
 # filenames = glob('Fri_Jun_30_*.npz')
 # These are the useful ones
 # [s + mystring for s in mylist]
-filenames = ['45:26', '46:08', '47:56', '48:18', '54:16', '55:12', '56:08', '56:44', '58:49']
-filenames = ['ExampleData/Fri_Jun_30_15:' + s for s in filenames]
-filenames = [s + '_2017.npz' for s in filenames]
+filenames = [
+    "45:26",
+    "46:08",
+    "47:56",
+    "48:18",
+    "54:16",
+    "55:12",
+    "56:08",
+    "56:44",
+    "58:49",
+]
+filenames = ["ExampleData/Fri_Jun_30_15:" + s for s in filenames]
+filenames = [s + "_2017.npz" for s in filenames]
 
 # %%
 
@@ -38,15 +48,15 @@ for i, filename in enumerate(filenames):
     d = dat[filename]
     plt.figure(i + 1)
     plt.clf()
-    plt.plot(d[d['aa']], d['pwr'] * 300. / 17.)
-    tit = ((filename.split('/')[1]).split('.')[0]).split('_')
-    plt.title(tit[0] + ' ' + tit[2] + ' ' + tit[1] + ' ' + tit[4] + ' ' + tit[3])
-    if d['aa'] == 'el':
-        Direction = 'Elevation'
+    plt.plot(d[d["aa"]], d["pwr"] * 300.0 / 17.0)
+    tit = ((filename.split("/")[1]).split(".")[0]).split("_")
+    plt.title(tit[0] + " " + tit[2] + " " + tit[1] + " " + tit[4] + " " + tit[3])
+    if d["aa"] == "el":
+        Direction = "Elevation"
     else:
-        Direction = 'Azimuth'
-    plt.xlabel(Direction + ' (deg)')
-    plt.ylabel(r'Antenna Temperature (K)')  # Power ($\mu$W)')
+        Direction = "Azimuth"
+    plt.xlabel(Direction + " (deg)")
+    plt.ylabel(r"Antenna Temperature (K)")  # Power ($\mu$W)')
 
 # Calibration seems to be roughly 300 K = 17 microW
 # El_true = El_measured + 4. (for the 15:56:03 data)
--- GUI/MRT_FUNC_PY4_GUI.py
+++ GUI/MRT_FUNC_PY4_GUI.py
@@ -10,6 +10,7 @@ import sys
 import time
 
 import MRTtools as mrt
+
 # import healpy as hp
 import matplotlib.pyplot as plt
 import mrtstate
